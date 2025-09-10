0085a9e662b4e2db617d810a657b825fcacbb617
gisse2007 <gisellquinterosolano@gmail.com>
2025-09-10 12:32:51 -0500

    API Clean

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import app from "./interfaces/server.js";
 const PORT = process.env.PORT || 3000;
 
 connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log(`Servidor corriendo en puerto: http://localhost:${PORT}`);
-    });
+  app.listen(PORT, () => {
+    console.log(` Servidor corriendo en puerto: ${PORT}`);
+  });
 });
